4965e628d635df07e71b315a6697e873e977935e
Alejo <alejojpa2@gmail.com>
2024-10-18 15:13:34 +0200

    debuuging 1

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeItem, updateQuantity } from './CartSlice';
 import './CartItem.css';
+import './ProductList';
 
 const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items); // 1) useSelector extracts data from Redux store! data stored as 'cart'.
