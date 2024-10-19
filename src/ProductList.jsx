5d05c8cae2794a03d43ecb0bf7aabdb6f8b8d6c6
Alejo <alejojpa2@gmail.com>
2024-10-19 10:44:19 +0200

    chaging setting at <h1 className='cart'>

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
-import { useDispatch} from 'react-redux'; // Added useDispatch import
-import {addItem} from './CartSlice'; //Reducer from CartSlice!!!
+import { useDispatch, useSelector} from 'react-redux'; // Added useDispatch import
+import {addItem, selectTotalQuantity} from './CartSlice'; //Reducer from CartSlice!!!
 
 
 function ProductList() {
@@ -262,7 +262,9 @@ function ProductList() {
     }));
     };
 
-    
+    // Fetch total quantity from the cart state
+    const totalQuantity = useSelector(selectTotalQuantity);
+    console.log("Total Quantity:", totalQuantity); // Debugging    
     
     return (
         <div>
@@ -285,6 +287,7 @@ function ProductList() {
                     <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
                         <h1 className='cart'>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
+                                <rect width="156" height="156" fill="red"></rect>
                                 <circle cx="80" cy="216" r="12"></circle>
                                 <circle cx="184" cy="216" r="12"></circle>
                                 <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
