a417e778c1bb47f80a81c9f1ce1b0dc7a93bd8e2
Alejo <alejojpa2@gmail.com>
2024-10-19 11:05:15 +0200

    adding   to the cart p2

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -291,7 +291,7 @@ function ProductList() {
                                 <circle cx="80" cy="216" r="12"></circle>
                                 <circle cx="184" cy="216" r="12"></circle>
                                 <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
-                                <text x="78" y="48" font-size="24" fill="white" text-anchor="middle" alignment-baseline="middle">${totalQuantity}</text>
+                                <text x="130" y="130" font-size="36" fill="white" text-anchor="middle" alignment-baseline="middle">{totalQuantity}</text>
                             </svg>
                         </h1>
                     </a>
